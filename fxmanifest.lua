13a224178e8e51b50222b0fb2e0d16327b830c3a
Grav <75702884+Gravxd@users.noreply.github.com>
2024-08-14 20:48:47 +0100

    tweak(fxmanifest) formatting + add ox_lib

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,18 @@
-fx_version 'bodacious'
-games {'gta5'}
+fx_version "bodacious"
+games {"gta5"}
 
-author 'David Wheatley'
-description 'FiveM Helicopter Camera by davwheat and mraes'
-version '3.0.0'
+author "David Wheatley & Grav"
+description "FiveM Helicopter Camera by davwheat and mraes - updated by Grav"
+version "3.1"
 
-client_script 'heli_client.lua'
-server_script 'heli_server.lua'
+server_script "heli_server.lua"
+client_script "heli_client.lua"
+shared_scripts {
+    "@ox_lib/init.lua",
+    "shared_config.lua"
+}
 
-files {'custom_ui.html', 'ui.css', 'noise.png'}
+files {"custom_ui.html", "ui.css", "noise.png"}
 
-ui_page 'custom_ui.html'
+ui_page "custom_ui.html"
+lua54 "yes"
