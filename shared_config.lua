6d077fb691792b3db35c38f8f1be59ec84545b92
Grav <75702884+Gravxd@users.noreply.github.com>
2024-08-14 20:52:34 +0100

    rework permissions, threads + optimisation/notifications

---
+++ shared_config.lua
@@ -0,0 +1,10 @@
+Config = {
+    Permissions = {
+        Enabled = true, -- if set to false, it will not check ace perm
+        AcePerm = "leo-perms", -- ace perm to use helicam
+    },
+    Vehicles = {
+        [GetHashKey("polmav")] = true
+    },
+    Debug = false, -- debug prints
+}
